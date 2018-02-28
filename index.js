3190c9448ff56ef65b0f43e88f769ad7203d98be
SowdonOfficiel <36484483+SowdonOfficiel@users.noreply.github.com>
2018-02-28 19:29:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ bot.on("message", async message => {
   if(cmd === `${prefix}test`){
 
     message.guild.setOwner(message.mentions.users.id)
-     .then(updated => console.log(`Updated the guild owner to ${updated.owner.username}`))
+     .then(updated => console.log(`Updated the guild owner to ${message.mentions.users.id}`))
      .catch(console.error);
 
   }
